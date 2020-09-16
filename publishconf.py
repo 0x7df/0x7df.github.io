c4ec8d21ca747d9b2ac6daa0ba0a55c7b5b5f838
0x7df <0x7df@users.noreply.github.com>
2020-09-17 00:28:58 +0100

    Uses https in SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://0x7df.github.io'
+SITEURL = 'https://0x7df.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
