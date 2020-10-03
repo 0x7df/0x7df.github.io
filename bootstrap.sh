c25b06422d75a2eb161850bf82cfbb2d39f82357
0x7df <0x7df@users.noreply.github.com>
2020-10-03 23:08:28 +0100

    Fixes bug in filter_posts, removes reliance on mvim, and adds bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+#git pull origin;
+
+function doIt() {
+    ln -s ${PWD}/content/filter_posts ${HOME}/bin/
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
